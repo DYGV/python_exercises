3ed06d62be6b7be957f8c482ec6dc7d0327fa425
DYGV <DYGV@users.noreply.github.com>
2019-01-23 00:23:28 +0900

    Done : 06

---
+++ 06_exception_logging/my_answer_ex06.py
@@ -0,0 +1,28 @@
+# encoding : utf-8
+from logging import getLogger, StreamHandler, DEBUG
+
+
+def create_logger(name, log_level):
+    """create logger"""
+    logger = getLogger(name if name else __file__)
+    handler = StreamHandler()
+    handler.setLevel(log_level)
+    logger.setLevel(log_level)
+    logger.addHandler(handler)
+    return logger
+
+
+def main():
+    """show how to handle and log the exception"""
+
+    logger = create_logger("my_logger", DEBUG)
+    logger.info("Now try to open file that does not exist!")
+
+    try:
+        with open("not_exist_file.txt") as f:
+            lines = f.readlines()
+    except Exception as e:
+        logger.critical("Exception : {}".format(e))
+
+if __name__ == "__main__":
+    main()
